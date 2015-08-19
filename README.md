dfb36797466ff2cba5fecc41b90bf0d79aa00be5
Peter Browse <peter@peterbrowse.com>
2015-08-19 18:59:56 +0100

    Adding in new node packages, updating tasks, updating index.js with package inclusions, updating package.json with dependencies.

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ This is the web worker for the [One Pixel Wide](http://www.onepixelwide.co.uk/) 
 It uses spotify web api audio previews and echonest data to sync gifs to any song on spotify.
 
 ## Tasks
+- [ ] Add in Spotify track request search call and return handler
+- [ ] Add in track selection call handler
+- [ ] Add in Echonest track details request handler
+- [ ] Add in function that calculates bpm to fps
+- [ ] Add in final object builder, which returns all the information required for the video to play and sync up animations etc.
 - [ ] Add cron job from standalone OPW server to ping this work every few hours
--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-var express 	= require('express'),
-	app 		= express();
+var express 		= require('express'),
+	echojs			= require('echojs'),
+	SpotifyWebApi 	= require('spotify-web-api-node'),
+	app 			= express();
 	
+var echonest = echojs({
+	key: process.env.ECHONEST_API_KEY
+});
+
+var spotify = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLEINT_SECRET,
+  redirectUri: APP_REDIRECT_CALLBACK_URL
+});
+
 	
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
 	response.send('Hello World!');
+});
+
+app.get('/callback', function(request, response) {
+	console.log(request);	
 });
 
 app.listen(app.get('port'), function() {
