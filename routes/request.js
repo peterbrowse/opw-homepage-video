1d29efe4e7e8a981a6d43057d5d299d08d99ada2
Peter Browse <peterbrowse@gmail.com>
2015-09-03 22:02:24 +0100

    Push.

--- routes/request.js
+++ routes/request.js
@@ -18,7 +18,7 @@ var spotifyApi = new SpotifyWebApi({
 
 router.get('/search', cors(), function(request, response, next) {
 	spotifyApi.searchTracks(request.query.track_search, { limit: 10 }).then(function(data) {
-		console.log('Search by ' + testSearch, data.body);
+		//console.log('Search by ' + testSearch, data.body);
 		response.status(200).type('json').send(data.body);
 	}, function(err) {
 		console.error(err);
